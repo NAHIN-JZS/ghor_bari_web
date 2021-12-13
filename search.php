3db2548f09feebf166e379c321585a6f85b2a773
Syed Nahin Hossain <syednahinhossain@gmail.com>
2021-12-13 15:27:34 +0600

    Search worked

--- search.php
+++ search.php
@@ -1,45 +1,26 @@
 <?php
-  //require 'config/config.php';
+  
+  include("nevigation.php");
+  require 'con2database.php';
+  
   $data = [];
   
   if(isset($_POST['search'])) {
     // Get data from FORM
     $location = $_POST['location'];
   
-  //location based search
-    $locations = explode(',', $location);
-    $loc = "(";
-    $numItems = count($locations);
-    $i = 0;
-    foreach ($locations as $key => $value) {
-      # code...
-      if(++$i === $numItems){
-         $loc .= "'".$value."'";
-      }else{
-        $loc .= "'".$value."',";
-      }
-    }
-    $loc .= ")";
+    $sql_search_ghor = "SELECT * from `house` INNER JOIN `user` ON `house`.`u_id` = `user`.`id` WHERE `house`.`city` = '$location'; ";
+    $data = mysqli_query($connect, $sql_search_ghor);
 
-  //end of location based search
+    if(isset($errMsg)){
+      echo '<div style="color:#FF0000;text-align:center;font-size:17px;">'.$errMsg.'</div>';
+    }
+    if(mysqli_num_rows($data) > 0){
+      echo "<h2 class='text-center'>List of Apartment Details</h2>";
+    }else{
+      echo "<h2 class='text-center' style='color:red;'>Try Some other keywords</h2>";
+    }
     
-    try {
-      //foreach ($keyword as $key => $value) {
-        # code...
-
-        $stmt = $connect->prepare("SELECT * FROM room_rental_registrations_apartment WHERE country IN $concats OR country IN $loc OR state IN $concats OR state IN $loc OR city IN $concats OR city IN $loc OR address IN $concats OR address IN $loc OR rooms IN $concats OR landmark IN $concats OR landmark IN $loc OR rent IN $concats OR deposit IN $concats");
-        $stmt->execute();
-        $data2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        $stmt = $connect->prepare("SELECT * FROM room_rental_registrations WHERE country IN $concats OR country IN $loc OR state IN $concats OR state IN $loc OR city IN $concats OR city IN $loc OR rooms IN $concats OR address IN $concats OR address IN $loc OR landmark IN $concats OR rent IN $concats OR deposit IN $concats");
-        $stmt->execute();
-        $data8 = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        $data = array_merge($data2, $data8);
-
-    }catch(PDOException $e) {
-      $errMsg = $e->getMessage();
-    }
   }
 ?>
 
@@ -56,10 +37,7 @@
 
 
 <body>
-<?php
-        include("nevigation.php");
-        require 'con2database.php';
-            ?> 
+
 <section id="search">
       <div class="container">
         <div class="row">
@@ -88,17 +66,7 @@
                 </div>
               </div>
             </form>
-
-            <?php
-              if(isset($errMsg)){
-                echo '<div style="color:#FF0000;text-align:center;font-size:17px;">'.$errMsg.'</div>';
-              }
-              if(count($data) !== 0){
-                echo "<h2 class='text-center'>List of Apartment Details</h2>";
-              }else{
-                //echo "<h2 class='text-center' style='color:red;'>Try Some other keywords</h2>";
-              }
-            ?>        
+      
             <?php 
                 foreach ($data as $key => $value) {           
                   echo '<div class="card card-inverse card-info mb-3" style="padding:1%;">          
@@ -107,11 +75,11 @@
                          echo   '<div class="row">
                             <div class="col-4">
                             <h4 class="text-center">Owner Details</h4>';
-                              echo '<p><b>Owner Name: </b>'.$value['fullname'].'</p>';
-                              echo '<p><b>Mobile Number: </b>'.$value['mobile'].'</p>';
-                              echo '<p><b>Alternate Number: </b>'.$value['alternat_mobile'].'</p>';
+                              echo '<p><b>Owner Name: </b>'.$value['name'].'</p>';
+                              echo '<p><b>Mobile Number: </b>'.$value['phone'].'</p>';
+                              //echo '<p><b>Alternate Number: </b>'.$value['alternat_mobile'].'</p>';
                               echo '<p><b>Email: </b>'.$value['email'].'</p>';
-                              echo '<p><b>Country: </b>'.$value['country'].'</p><p><b> State: </b>'.$value['state'].'</p><p><b> City: </b>'.$value['city'].'</p>';
+                              //echo '<p><b>Country: </b>'.$value['country'].'</p><p><b> State: </b>'.$value['state'].'</p><p><b> City: </b>'.$value['city'].'</p>';
                               if ($value['image'] !== 'uploads/') {
                                 # code...
                                 echo '<img src="app/'.$value['image'].'" width="100">';
@@ -121,30 +89,30 @@
                             <div class="col-5">
                             <h4 class="text-center">Room Details</h4>';
                               // echo '<p><b>Country: </b>'.$value['country'].'<b> State: </b>'.$value['state'].'<b> City: </b>'.$value['city'].'</p>';
-                              echo '<p><b>Plot Number: </b>'.$value['plot_number'].'</p>';
+                              echo '<p><b>House Number: </b>'.$value['house_no'].'</p>';
 
-                              if(isset($value['sale'])){
-                                echo '<p><b>Sale: </b>'.$value['sale'].'</p>';
+                              if(isset($value['cost'])){
+                                echo '<p><b>Rent Fare: </b>'.$value['cost'].'</p>';
                               } 
                               
-                                if(isset($value['apartment_name']))                         
-                                  echo '<div class="alert alert-success" role="alert"><p><b>Apartment Name: </b>'.$value['apartment_name'].'</p></div>';
+                                //if(isset($value['apartment_name']))                         
+                                  //echo '<div class="alert alert-success" role="alert"><p><b>Apartment Name: </b>'.$value['apartment_name'].'</p></div>';
 
-                                if(isset($value['ap_number_of_plats']))
-                                  echo '<div class="alert alert-success" role="alert"><p><b>Plat Number: </b>'.$value['ap_number_of_plats'].'</p></div>';
+                                //if(isset($value['ap_number_of_plats']))
+                                  //echo '<div class="alert alert-success" role="alert"><p><b>Plat Number: </b>'.$value['ap_number_of_plats'].'</p></div>';
 
-                              echo '<p><b>Available Rooms: </b>'.$value['rooms'].'</p>';
-                              echo '<p><b>Address: </b>'.$value['address'].'</p><p><b> Landmark: </b>'.$value['landmark'].'</p>';
+                              echo '<p><b>Available Rooms: </b>'.$value['total_room'].'</p>';
+                              echo '<p><b>Address: </b>'.$value['address'].'</p>';
                           echo '</div>
                             <div class="col-3">
                             <h4>Other Details</h4>';
-                            echo '<p><b>Accommodation: </b>'.$value['accommodation'].'</p>';
-                            echo '<p><b>Description: </b>'.$value['description'].'</p>';
-                              if($value['vacant'] == 0){ 
-                                echo '<div class="alert alert-danger" role="alert"><p><b>Occupied</b></p></div>';
-                              }else{
-                                echo '<div class="alert alert-success" role="alert"><p><b>Vacant</b></p></div>';
-                              } 
+                            //echo '<p><b>Accommodation: </b>'.$value['accommodation'].'</p>';
+                            echo '<p><b>Description: </b>'.$value['additional_info'].'</p>';
+                              //if($value['vacant'] == 0){ 
+                                //echo '<div class="alert alert-danger" role="alert"><p><b>Occupied</b></p></div>';
+                              //}else{
+                               // echo '<div class="alert alert-success" role="alert"><p><b>Vacant</b></p></div>';
+                             // } 
                             echo '</div>
                           </div>              
                          </div>
